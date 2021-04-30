18063cd83be82b8b49e761b3ec528d515f3e6724
Terry Pawn <terry.pawn@tranchess.com>
2021-04-30 03:24:03 +0000

    Remove unused contract

--- contracts/interfaces/IToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.10 <0.8.0;
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
-interface IToken {
-    function mint(address account, uint256 amount) external returns (bool);
-}
