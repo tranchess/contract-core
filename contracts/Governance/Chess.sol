094a41f4dbe9c9789ce3d4b10d180c7e2a1fadef
Bill Clippy <bill.clippy@tranchess.com>
2021-05-09 23:53:00 -0700

    add hardcode bytes representing the entire supply schedule and basic testing

--- contracts/Governance/Chess.sol
+++ contracts/Governance/Chess.sol
@@ -13,19 +13,20 @@ contract Chess is IChess, Ownable, ERC20, ChessRoles {
     using SafeDecimalMath for uint256;
 
     // Supply parameters
-    uint256[7] public schedule = [
-        100e18, // Initial Supply
-        100e18, // Week 0 Supply === Initial Supply
-        110e18, // Week 1 Supply
-        120e18, // ...
-        130e18,
-        140e18,
-        150e18 // Week 5 Supply
-    ];
+    // prettier-ignore
+    bytes public constant schedule = 
+        hex"0000000000000000000000000000000000000000000000056bc75e2d63100000"
+        hex"0000000000000000000000000000000000000000000000056bc75e2d63100000"
+        hex"000000000000000000000000000000000000000000000005f68e8131ecf80000"
+        hex"0000000000000000000000000000000000000000000000068155a43676e00000"
+        hex"0000000000000000000000000000000000000000000000070c1cc73b00c80000"
+        hex"00000000000000000000000000000000000000000000000796e3ea3f8ab00000"
+        hex"00000000000000000000000000000000000000000000000821ab0d4414980000";
+
     uint256 public startTimestamp;
 
     constructor(uint256 _startTimestamp) public ERC20("Chess", "CHESS") ChessRoles() {
-        _mint(msg.sender, schedule[0]);
+        _mint(msg.sender, getScheduledSupply(0));
         startTimestamp = _startTimestamp;
     }
 
@@ -39,6 +40,30 @@ contract Chess is IChess, Ownable, ERC20, ChessRoles {
         return (timestamp - startTimestamp) / 1 weeks;
     }
 
+    function getScheduledSupply(uint256 index) public pure returns (uint256 value) {
+        bytes memory scheduleBytes = bytes(schedule);
+        uint256 offset = (index + 1) * 32;
+        assembly {
+            value := mload(add(scheduleBytes, offset))
+        }
+    }
+
+    function getWeeklySupply(uint256 index)
+        public
+        pure
+        returns (uint256 currentWeekSupply, uint256 weeklySupply)
+    {
+        bytes memory scheduleBytes = bytes(schedule);
+        uint256 offset = (index + 1) * 32;
+        uint256 nextWeekSupply;
+        assembly {
+            currentWeekSupply := mload(add(scheduleBytes, offset))
+            nextWeekSupply := mload(add(scheduleBytes, add(offset, 32)))
+        }
+
+        weeklySupply = nextWeekSupply.sub(currentWeekSupply);
+    }
+
     /// @notice Current number of tokens in existence (claimed or unclaimed)
     function availableSupply() public view returns (uint256) {
         return _availableSupply();
@@ -49,7 +74,7 @@ contract Chess is IChess, Ownable, ERC20, ChessRoles {
     /// @return Release rate (number of CHESS token per second)
     function getRate(uint256 timestamp) external view override returns (uint256) {
         uint256 index = getIndex(timestamp);
-        uint256 weeklySupply = schedule[index + 1].sub(schedule[index]);
+        (, uint256 weeklySupply) = getWeeklySupply(index);
         return weeklySupply.div(1 weeks);
     }
 
@@ -73,7 +98,7 @@ contract Chess is IChess, Ownable, ERC20, ChessRoles {
     function _availableSupply() internal view returns (uint256) {
         uint256 index = getIndex(block.timestamp);
         uint256 currentWeek = index * 1 weeks + startTimestamp;
-        uint256 weeklySupply = schedule[index + 1].sub(schedule[index]);
-        return schedule[index].add(weeklySupply.mul(block.timestamp - currentWeek).div(1 weeks));
+        (uint256 currentWeekSupply, uint256 weeklySupply) = getWeeklySupply(index);
+        return currentWeekSupply.add(weeklySupply.mul(block.timestamp - currentWeek).div(1 weeks));
     }
 }
---
+++ test/chess.ts
@@ -0,0 +1,78 @@
+import { expect } from "chai";
+import { BigNumber, Contract, Wallet } from "ethers";
+import type { Fixture, MockContract, MockProvider } from "ethereum-waffle";
+import { waffle, ethers } from "hardhat";
+const { loadFixture } = waffle;
+const { parseEther } = ethers.utils;
+import { deployMockForName } from "./mock";
+
+const DAY = 86400;
+const WEEK = DAY * 7;
+
+async function advanceBlockAtTime(time: number) {
+    await ethers.provider.send("evm_mine", [time]);
+}
+
+describe("Ballot", function () {
+    interface FixtureWalletMap {
+        readonly [name: string]: Wallet;
+    }
+
+    interface FixtureData {
+        readonly wallets: FixtureWalletMap;
+        readonly startWeek: number;
+        readonly chess: Contract;
+    }
+
+    let currentFixture: Fixture<FixtureData>;
+    let fixtureData: FixtureData;
+
+    let startWeek: number;
+    let user1: Wallet;
+    let user2: Wallet;
+    let owner: Wallet;
+    let addr1: string;
+    let chess: Contract;
+
+    async function deployFixture(_wallets: Wallet[], provider: MockProvider): Promise<FixtureData> {
+        for (let i = 100; i < 160; i += 10) {
+            console.log(BigNumber.from(i).mul(BigNumber.from(10).pow(18)).toHexString());
+        }
+        const [user1, user2, owner] = provider.getWallets();
+
+        // Start at the midnight in the next Thursday.
+        const startTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+        const startWeek = Math.ceil(startTimestamp / WEEK) * WEEK + WEEK;
+
+        const Chess = await ethers.getContractFactory("Chess");
+        const chess = await Chess.connect(owner).deploy(startWeek);
+
+        return {
+            wallets: { user1, user2, owner },
+            startWeek,
+            chess: chess.connect(user1),
+        };
+    }
+
+    before(function () {
+        currentFixture = deployFixture;
+    });
+
+    beforeEach(async function () {
+        fixtureData = await loadFixture(currentFixture);
+        user1 = fixtureData.wallets.user1;
+        user2 = fixtureData.wallets.user2;
+        owner = fixtureData.wallets.owner;
+        addr1 = user1.address;
+        startWeek = fixtureData.startWeek;
+        chess = fixtureData.chess;
+    });
+
+    describe("getScheduledSupply()", function () {
+        it("Should get scheduled supply", async function () {
+            expect(await chess.getScheduledSupply(0)).to.equal(BigNumber.from(10).pow(18).mul(100));
+            expect(await chess.getScheduledSupply(1)).to.equal(BigNumber.from(10).pow(18).mul(100));
+            expect(await chess.getScheduledSupply(2)).to.equal(BigNumber.from(10).pow(18).mul(110));
+        });
+    });
+});
