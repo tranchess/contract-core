d0723e0165d605d1210b6f86c0f9b73d5720a707
Bill Clippy <bill.clippy@tranchess.com>
2021-05-13 22:10:20 -0700

    remove mnemonics in solcover file

--- .core.solcover.js
+++ .core.solcover.js
@@ -2,9 +2,5 @@ module.exports = {
     norpc: true,
     testCommand: "npm test",
     compileCommand: "npm run compile",
-    providerOptions: {
-        mnemonic:
-            "tuition produce fat desk suggest case essence wreck warfare convince razor bless",
-    },
     skipFiles: ["utils", "test", "interfaces", "misc"],
 };
