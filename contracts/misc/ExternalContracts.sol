de117e31d997aeb588a598b760e55412a0aad4ea
Terry Pawn <terry.pawn@tranchess.com>
2021-05-19 14:17:26 +0800

    Rename LibraryImports to ExternalContracts

--- contracts/misc/ExternalContracts.sol
+++ contracts/misc/ExternalContracts.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.10 <0.8.0;
 
-// This file imports library contracts that are used by this project,
+// This file imports external contracts that are used by this project,
 // forcing Hardhat to compile them.
 
 import "@openzeppelin/contracts/access/TimelockController.sol";
