a9b39e7f5677aeff191cbee51473b093d94739dd
Terry Pawn <terry.pawn@tranchess.com>
2021-06-24 10:52:30 +0800

    Add deployment task for vesting escrow

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import "./tasks/deploy_governance";
 import "./tasks/deploy_misc";
 import "./tasks/deploy_mock";
 import "./tasks/deploy_oracle";
+import "./tasks/deploy_vesting";
 import "./tasks/test_deploy";
 import { ETH_RPC, ETH_CHAIN_ID, DEPLOYER_PK, DEPLOYER_HD_PATH, ETHERSCAN_API_KEY } from "./config";
 import "hardhat-gas-reporter";
