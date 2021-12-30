99a6024102338fb524d05fc7d7c51b73e62e72eb
Terry Pawn <terry.pawn@tranchess.com>
2021-12-30 14:40:15 +0800

    Refactor controller V3

--- contracts/governance/ChessControllerV3.sol
+++ contracts/governance/ChessControllerV3.sol
@@ -22,19 +22,19 @@ contract ChessControllerV3 is IChessController, CoreUtility {
 
     address public immutable fund0;
     address public immutable fund1;
-    mapping(uint256 => mapping(address => uint256)) public weights;
+    address public immutable fund2;
 
     uint256 public immutable guardedLaunchStart;
+    uint256 public immutable guardedLaunchStartV3;
 
-    address public immutable fund2;
-    uint256 public immutable guardedLaunchStart2;
+    mapping(uint256 => mapping(address => uint256)) public weights;
 
     constructor(
         address fund0_,
         address fund1_,
         address fund2_,
         uint256 guardedLaunchStart_,
-        uint256 guardedLaunchStart2_,
+        uint256 guardedLaunchStartV3_,
         uint256 minWeight_
     ) public {
         require(minWeight_ > 0 && minWeight_ < 1e18);
@@ -42,39 +42,22 @@ contract ChessControllerV3 is IChessController, CoreUtility {
         fund1 = fund1_;
         fund2 = fund2_;
         guardedLaunchStart = guardedLaunchStart_;
-        guardedLaunchStart2 = guardedLaunchStart2_;
+        require(_endOfWeek(guardedLaunchStart_) == guardedLaunchStart_ + 1 weeks);
+        guardedLaunchStartV3 = guardedLaunchStartV3_;
+        require(_endOfWeek(guardedLaunchStartV3_) == guardedLaunchStartV3_ + 1 weeks);
+        require(guardedLaunchStartV3_ > guardedLaunchStart_);
         minWeight = minWeight_;
     }
 
-    function initialize(uint256[] calldata guardedWeights0_) external {
-        require(weights[guardedLaunchStart][fund0] == 0);
-        require(guardedWeights0_.length > 0);
-        require(_endOfWeek(guardedLaunchStart) == guardedLaunchStart + 1 weeks, "Not end of week");
-        for (uint256 i = 0; i < guardedWeights0_.length; i++) {
-            uint256 guardedWeight0 = guardedWeights0_[i];
-            require(
-                guardedWeight0 >= minWeight && guardedWeight0 <= 1e18 - minWeight,
-                "Invalid weight"
-            );
-            weights[guardedLaunchStart + i * 1 weeks][fund0] = guardedWeight0;
-            weights[guardedLaunchStart + i * 1 weeks][fund1] = 1e18 - guardedWeight0;
-        }
-    }
-
-    function initializeV2(uint256[] calldata guardedWeights0_) external {
-        require(weights[guardedLaunchStart2][fund2] == 0);
-        require(guardedWeights0_.length > 0);
-        require(
-            _endOfWeek(guardedLaunchStart2) == guardedLaunchStart2 + 1 weeks,
-            "Not end of week"
-        );
-        for (uint256 i = 0; i < guardedWeights0_.length; i++) {
-            uint256 guardedWeight0 = guardedWeights0_[i];
-            require(
-                guardedWeight0 >= minWeight && guardedWeight0 <= 1e18 - minWeight,
-                "Invalid weight"
-            );
-            weights[guardedLaunchStart2 + i * 1 weeks][fund2] = guardedWeight0;
+    function initializeV3(uint256[] calldata guardedWeights2_) external {
+        require(guardedLaunchStartV3 > block.timestamp, "Too late to initialize");
+        require(weights[guardedLaunchStartV3][fund0] == 0); // Make sure guarded launch in V2 has ended.
+        require(weights[guardedLaunchStartV3][fund2] == 0, "Already initialized");
+        require(guardedWeights2_.length > 0);
+        for (uint256 i = 0; i < guardedWeights2_.length; i++) {
+            uint256 weight2 = guardedWeights2_[i];
+            require(weight2 >= minWeight && weight2 <= 1e18 - minWeight * 2, "Invalid weight");
+            weights[guardedLaunchStartV3 + i * 1 weeks][fund2] = weight2;
         }
     }
 
@@ -90,7 +73,9 @@ contract ChessControllerV3 is IChessController, CoreUtility {
         if (fundAddress != fund0 && fundAddress != fund1 && fundAddress != fund2) {
             return 0;
         }
-        if (timestamp < guardedLaunchStart2 && fundAddress == fund2) {
+        if (timestamp < guardedLaunchStart) {
+            return fundAddress == fund0 ? 1e18 : 0;
+        } else if (timestamp < guardedLaunchStartV3 && fundAddress == fund2) {
             return 0;
         }
 
@@ -101,70 +86,53 @@ contract ChessControllerV3 is IChessController, CoreUtility {
         }
 
         (uint256 weight0, uint256 weight1, uint256 weight2) = _updateFundWeight(weekTimestamp);
-        if (fundAddress == fund0) return weight0;
-        else if (fundAddress == fund1) return weight1;
-        else return weight2;
+        if (fundAddress == fund0) {
+            return weight0;
+        } else if (fundAddress == fund1) {
+            return weight1;
+        } else {
+            return weight2;
+        }
     }
 
     function _updateFundWeight(uint256 weekTimestamp)
         private
         returns (
-            uint256 weightMovingAverage0,
-            uint256 weightMovingAverage1,
-            uint256 weightMovingAverage2
+            uint256 weight0,
+            uint256 weight1,
+            uint256 weight2
         )
     {
-        uint256 currFundWeight2 = weights[weekTimestamp][fund2];
+        uint256 prevWeight0 = weights[weekTimestamp - 1 weeks][fund0];
+        require(prevWeight0 != 0, "Previous week is empty");
+        weight2 = weights[weekTimestamp][fund2];
+        if (weight2 == 0) {
+            // After guarded launch V3, keep weight of fund 2 constant. This contract is planned to
+            // be upgraded again after guarded launch V3 and the constant weight2 won't last long.
+            weight2 = weights[weekTimestamp - 1 weeks][fund2];
+        }
         uint256 fundValueLocked0 = getFundValueLocked(fund0, weekTimestamp);
-        uint256 fundValueLocked1 = getFundValueLocked(fund1, weekTimestamp);
-        uint256 totalValueLocked =
-            fundValueLocked0.add(fundValueLocked1).add(getFundValueLocked(fund2, weekTimestamp));
-
-        if (totalValueLocked == 0) {
-            uint256 prevFundWeight0 = weights[weekTimestamp - 1 weeks][fund0];
-            uint256 prevFundWeight1 = weights[weekTimestamp - 1 weeks][fund1];
-            require(prevFundWeight0 != 0 && prevFundWeight1 != 0, "Previous week is empty");
-
-            weightMovingAverage0 = prevFundWeight0;
-            weightMovingAverage1 = prevFundWeight1;
-            weightMovingAverage2 = weights[weekTimestamp - 1 weeks][fund2];
-        } else if (currFundWeight2 != 0) {
-            // guarded launch
-            uint256 prevFundWeight0 = weights[weekTimestamp - 1 weeks][fund0];
-            totalValueLocked = fundValueLocked0.add(fundValueLocked1);
-            require(prevFundWeight0 != 0, "Previous week is empty");
-
-            weightMovingAverage0 = (prevFundWeight0.mul(WINDOW_SIZE - 1).add(
-                fundValueLocked0.divideDecimal(totalValueLocked)
-            ) / WINDOW_SIZE)
-                .max(minWeight)
-                .min(1e18 - minWeight)
-                .mul(1e18 - currFundWeight2)
-                .div(1e18);
-            weightMovingAverage1 = uint256(1e18).sub(currFundWeight2).sub(weightMovingAverage0);
-        } else {
-            uint256 prevFundWeight0 = weights[weekTimestamp - 1 weeks][fund0];
-            uint256 prevFundWeight1 = weights[weekTimestamp - 1 weeks][fund1];
-            require(prevFundWeight0 != 0 && prevFundWeight1 != 0, "Previous week is empty");
-
-            weightMovingAverage0 = (prevFundWeight0.mul(WINDOW_SIZE - 1).add(
-                fundValueLocked0.divideDecimal(totalValueLocked)
-            ) / WINDOW_SIZE)
-                .max(minWeight)
-                .min(1e18 - minWeight);
-            weightMovingAverage1 = (prevFundWeight1.mul(WINDOW_SIZE - 1).add(
-                fundValueLocked1.divideDecimal(totalValueLocked)
-            ) / WINDOW_SIZE)
-                .max(minWeight)
-                .min(1e18 - minWeight);
-            weightMovingAverage2 = uint256(1e18).sub(weightMovingAverage0).sub(
-                weightMovingAverage1
+        uint256 totalValueLocked = fundValueLocked0.add(getFundValueLocked(fund1, weekTimestamp));
+        if (weekTimestamp == guardedLaunchStartV3) {
+            prevWeight0 = prevWeight0.multiplyDecimal(1e18 - weight2).max(minWeight).min(
+                1e18 - weight2 - minWeight
             );
         }
 
-        weights[weekTimestamp][fund0] = weightMovingAverage0;
-        weights[weekTimestamp][fund1] = weightMovingAverage1;
-        weights[weekTimestamp][fund2] = weightMovingAverage2;
+        if (totalValueLocked == 0) {
+            weight0 = prevWeight0;
+        } else {
+            weight0 = (prevWeight0.mul(WINDOW_SIZE - 1).add(
+                fundValueLocked0.divideDecimal(totalValueLocked).multiplyDecimal(1e18 - weight2)
+            ) / WINDOW_SIZE)
+                .max(minWeight)
+                .min(1e18 - weight2 - minWeight);
+        }
+        weight1 = 1e18 - weight2 - weight0;
+
+        weights[weekTimestamp][fund0] = weight0;
+        weights[weekTimestamp][fund1] = weight1;
+        weights[weekTimestamp][fund2] = weight2;
     }
 
     function getFundValueLocked(address fund, uint256 weekTimestamp)
