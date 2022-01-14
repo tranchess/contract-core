97f280fd2ed9954434e9cae6fa614b014b98a7b0
Terry Pawn <terry.pawn@tranchess.com>
2022-01-14 14:45:58 +0800

    Add a configurable delay in keeper contracts

--- contracts/keeper/FundKeeperHelper.sol
+++ contracts/keeper/FundKeeperHelper.sol
@@ -13,23 +13,31 @@ interface IDistributor {
 }
 
 contract FundKeeperHelper is BatchKeeperHelperBase {
+    uint256 public delay;
+
     address private immutable _bnbFundAddr;
     IDistributor private immutable _feeDistributor;
 
     constructor(
         address[] memory funds_,
+        uint256 delay_,
         address bnbFundAddr_,
         address feeDistributor_
     ) public BatchKeeperHelperBase(funds_) {
+        delay = delay_;
         _bnbFundAddr = bnbFundAddr_;
         _feeDistributor = IDistributor(feeDistributor_);
+    }
+
+    function updateDelay(uint256 newDelay) external onlyOwner {
+        delay = newDelay;
     }
 
     function _checkUpkeep(address contractAddress) internal override returns (bool) {
         IFundSettlement fund = IFundSettlement(contractAddress);
         uint256 currentDay = fund.currentDay();
         uint256 price = fund.twapOracle().getTwap(currentDay);
-        return (block.timestamp >= currentDay && price != 0);
+        return (block.timestamp >= currentDay + delay && price != 0);
     }
 
     function _performUpkeep(address contractAddress) internal override {
