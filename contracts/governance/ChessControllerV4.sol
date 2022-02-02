0550b7ce33efa5ed18cd5b7ab4276dca0ff332fc
Terry Pawn <terry.pawn@tranchess.com>
2022-02-03 00:51:23 +0800

    Add last timestamp in ChessControllerV4 and add test

--- contracts/governance/ChessControllerV4.sol
+++ contracts/governance/ChessControllerV4.sol
@@ -18,11 +18,16 @@ contract ChessControllerV4 is IChessController, CoreUtility {
     using SafeMath for uint256;
     using SafeDecimalMath for uint256;
 
+    event WeightUpdated(address indexed fund, uint256 indexed timestamp, uint256 weight);
+
     address public immutable fund0;
     uint256 public immutable guardedLaunchStart;
     address public immutable controllerBallot;
 
     mapping(uint256 => mapping(address => uint256)) public weights;
+
+    /// @notice Start timestamp of the last trading week that has weights updated.
+    uint256 public lastTimestamp;
 
     constructor(
         address fund0_,
@@ -33,6 +38,26 @@ contract ChessControllerV4 is IChessController, CoreUtility {
         guardedLaunchStart = guardedLaunchStart_;
         require(_endOfWeek(guardedLaunchStart_) == guardedLaunchStart_ + 1 weeks);
         controllerBallot = controllerBallot_;
+    }
+
+    /// @dev Initialize the part added in V4. The contract is designed to be used with OpenZeppelin's
+    ///      `TransparentUpgradeableProxy`. If this contract is upgraded from the previous version,
+    ///      call `upgradeToAndCall` of the proxy and put a call to this function in the `data`
+    ///      argument with `lastTimestamp_` set to the last updated week. If this contract is
+    ///      the first implementation of the proxy, This function should be called by the proxy's
+    ///      constructor (via the `_data` argument) with `lastTimestamp_` set to one week before
+    ///      `guardedLaunchStart`.
+    function initializeV4(uint256 lastTimestamp_) external {
+        require(lastTimestamp == 0, "Already initialized");
+        require(
+            _endOfWeek(lastTimestamp_) == lastTimestamp_ + 1 weeks &&
+                lastTimestamp_ >= guardedLaunchStart - 1 weeks
+        );
+        require(weights[lastTimestamp_ + 1 weeks][fund0] == 0, "Next week already updated");
+        if (lastTimestamp_ >= guardedLaunchStart) {
+            require(weights[lastTimestamp_][fund0] > 0, "Last week not updated");
+        }
+        lastTimestamp = lastTimestamp_;
     }
 
     /// @notice Get Fund relative weight (not more than 1.0) normalized to 1e18
@@ -47,13 +72,13 @@ contract ChessControllerV4 is IChessController, CoreUtility {
         if (timestamp < guardedLaunchStart) {
             return fundAddress == fund0 ? 1e18 : 0;
         }
-
         uint256 weekTimestamp = _endOfWeek(timestamp).sub(1 weeks);
-        uint256 weight = weights[weekTimestamp][fundAddress];
-        if (weight != 0) {
-            return weight;
+        uint256 lastTimestamp_ = lastTimestamp; // gas saver
+        require(weekTimestamp <= lastTimestamp_ + 1 weeks, "Previous week is empty");
+        if (weekTimestamp <= lastTimestamp_) {
+            return weights[weekTimestamp][fundAddress];
         }
-
+        lastTimestamp = lastTimestamp_ + 1 weeks;
         return _updateFundWeight(weekTimestamp, fundAddress);
     }
 
@@ -78,6 +103,7 @@ contract ChessControllerV4 is IChessController, CoreUtility {
                 fundWeight = fundWeight.add(fundValueLocked[i].divideDecimal(totalValueLocked)) / 2;
             }
             weights[weekTimestamp][funds[i]] = fundWeight;
+            emit WeightUpdated(funds[i], weekTimestamp, fundWeight);
             if (funds[i] == fundAddress) {
                 weight = fundWeight;
             }
