8bba1d0faf6d27dc1fb0838e2d1d433efafa4d2f
Terry Pawn <terry.pawn@tranchess.com>
2022-09-14 16:42:28 +0800

    Fix weekly supply and rate in ChessSubSchedule

--- contracts/governance/ChessSubSchedule.sol
+++ contracts/governance/ChessSubSchedule.sol
@@ -44,15 +44,21 @@ contract ChessSubSchedule is IChessSchedule, Ownable, ChessRoles, CoreUtility, A
         chess = IAnyswapV6ERC20(chess_);
     }
 
+    /// @notice Get supply of the week containing the given timestamp. This function usually
+    ///         returns zero when called at the beginning of the week. After cross-chain CHESS
+    ///         emission is delivered to this contract, this function returns the correct value.
     function getWeeklySupply(uint256 timestamp) public view override returns (uint256) {
-        return _weeklySupplies[timestamp];
+        return _weeklySupplies[_endOfWeek(timestamp) - 1 weeks];
     }
 
-    /// @notice Get the release rate of CHESS token at the given timestamp
+    /// @notice Get the release rate of CHESS token at the given timestamp. This function usually
+    ///         returns zero when called at the beginning of the week. After cross-chain CHESS
+    ///         emission is delivered to this contract, this function returns the average rate over
+    ///         a whole week.
     /// @param timestamp Timestamp for release rate
     /// @return Release rate (number of CHESS token per second)
     function getRate(uint256 timestamp) external view override returns (uint256) {
-        return _weeklySupplies[timestamp].div((timestamp + 1) * 1 weeks - block.timestamp);
+        return _weeklySupplies[_endOfWeek(timestamp) - 1 weeks] / 1 weeks;
     }
 
     /// @notice Creates `amount` CHESS tokens and assigns them to `account`,
@@ -73,6 +79,7 @@ contract ChessSubSchedule is IChessSchedule, Ownable, ChessRoles, CoreUtility, A
         _removeMinter(account);
     }
 
+    /// @notice Send the total veCHESS amount voted to all pools on this chain to the main chain.
     function crossChainSync() external payable {
         uint256 week = _endOfWeek(block.timestamp);
         uint256 supply = controllerBallot.totalSupplyAtWeek(week);
@@ -88,14 +95,14 @@ contract ChessSubSchedule is IChessSchedule, Ownable, ChessRoles, CoreUtility, A
         return from == scheduleRelayer && fromChainID == mainChainID;
     }
 
-    /// @dev Crosschain chess emission (step 4)
-    /// Receive emissions from mainchain
+    /// @dev Receive CHESS emission from the main chain.
     function _anyExecute(
-        uint256, /*fromChainID*/
+        uint256, // fromChainID
         bytes calldata data
     ) internal override {
         uint256 totalAmount = abi.decode(data, (uint256));
         uint256 currentWeek = _endOfWeek(block.timestamp) - 1 weeks;
+        require(_weeklySupplies[currentWeek] == 0, "Already received");
         availableSupply = availableSupply.add(totalAmount);
         _weeklySupplies[currentWeek] = totalAmount;
     }
