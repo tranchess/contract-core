87a8e71189c2e4b22f9516f6ac104e1f004e2ac9
Terry Pawn <terry.pawn@tranchess.com>
2022-10-15 18:18:22 +0800

    Remove pool addr from CurveRouter ctor

--- contracts/swap/CurveRouter.sol
+++ contracts/swap/CurveRouter.sol
@@ -19,23 +19,29 @@ interface ICurvePool {
     ) external payable returns (uint256);
 }
 
+interface ICurveLiquidityToken {
+    function minter() external view returns (address);
+}
+
 contract CurveRouter {
     using SafeERC20 for IERC20;
 
-    ICurvePool public immutable curvePool;
-    address public immutable WETH20;
     LiquidityGaugeCurve public immutable tranchessLiquidityGauge;
     IERC20 public immutable curveLiquidityToken;
+    ICurvePool public immutable curvePool;
+    address public immutable WETH20;
     address[2] public coins;
 
-    constructor(address curvePool_, address tranchessLiquidityGauge_) public {
-        curvePool = ICurvePool(curvePool_);
-        WETH20 = ICurvePool(curvePool_).WETH20();
-        coins[0] = ICurvePool(curvePool_).coins(0);
-        coins[1] = ICurvePool(curvePool_).coins(1);
-
+    constructor(address tranchessLiquidityGauge_) public {
         tranchessLiquidityGauge = LiquidityGaugeCurve(tranchessLiquidityGauge_);
-        curveLiquidityToken = LiquidityGaugeCurve(tranchessLiquidityGauge_).curveLiquidityToken();
+        IERC20 liquidityToken = LiquidityGaugeCurve(tranchessLiquidityGauge_).curveLiquidityToken();
+        curveLiquidityToken = liquidityToken;
+
+        ICurvePool pool = ICurvePool(ICurveLiquidityToken(address(liquidityToken)).minter());
+        curvePool = pool;
+        WETH20 = pool.WETH20();
+        coins[0] = pool.coins(0);
+        coins[1] = pool.coins(1);
     }
 
     receive() external payable {}
