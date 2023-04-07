5dc96ae7eaf54f38e8d3f325f63dfd2090fd96c3
Terry Pawn <terry.pawn@tranchess.com>
2023-04-07 10:03:16 +0800

    Remove unused code

--- contracts/nonfungible/NonfungibleRedemptionDescriptor.sol
+++ contracts/nonfungible/NonfungibleRedemptionDescriptor.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.6.10 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "base64-sol/base64.sol";
 
