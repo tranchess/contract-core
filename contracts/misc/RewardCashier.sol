6825bba9919c45a11e0993ce74314498bae8373f
Bill Clippy <bill.clippy@tranchess.com>
2023-04-25 00:04:52 -0400

    Add deadline

--- contracts/misc/RewardCashier.sol
+++ contracts/misc/RewardCashier.sol
@@ -3,17 +3,23 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
+import "../utils/SafeDecimalMath.sol";
 
 contract RewardCashier is Ownable {
+    using SafeDecimalMath for uint256;
+
     address public immutable token;
+    uint256 public immutable deadline;
 
     mapping(uint256 => bytes32) public roots;
+    mapping(uint256 => uint256) public ratios;
     mapping(address => uint256) public claimed;
 
     uint256 public currentVersion;
 
-    constructor(address token_) public {
+    constructor(address token_, uint256 deadline_) public {
         token = token_;
+        deadline = deadline_;
     }
 
     function claim(
@@ -22,6 +28,7 @@ contract RewardCashier is Ownable {
         uint256 version,
         bytes32[] calldata merkleProof
     ) external {
+        require(block.timestamp < deadline, "Deadline passed");
         require(claimed[account] < version, "Already claimed");
         require(version > 0 && version <= currentVersion, "Invalid version");
 
@@ -29,7 +36,8 @@ contract RewardCashier is Ownable {
         require(checkValidity(merkleProof, roots[version], leaf), "Invalid proof");
 
         claimed[account] = version;
-        IERC20(token).transfer(account, amount);
+        uint256 reward = amount.multiplyDecimal(ratios[version]);
+        IERC20(token).transfer(account, reward);
     }
 
     function checkValidity(
@@ -41,8 +49,18 @@ contract RewardCashier is Ownable {
         return true;
     }
 
-    function addNewRoot(bytes32 root_) external onlyOwner {
+    function addNewRoot(
+        bytes32 root,
+        uint256 totalRewards,
+        uint256 totalShares
+    ) external onlyOwner {
         currentVersion++;
-        roots[currentVersion] = root_;
+        roots[currentVersion] = root;
+        ratios[currentVersion] = totalRewards.divideDecimal(totalShares);
+    }
+
+    function drain() external onlyOwner {
+        require(block.timestamp >= deadline);
+        IERC20(token).transfer(owner(), IERC20(token).balanceOf(address(this)));
     }
 }
