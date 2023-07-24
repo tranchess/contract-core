3e33a2250cb624500ccff00bfad6019950cee08e
Bill Clippy <bill.clippy@tranchess.com>
2023-07-24 10:36:14 -0700

    Check lastWeek in relayer

--- contracts/keeper/CrossChainMintKeeperHelper.sol
+++ contracts/keeper/CrossChainMintKeeperHelper.sol
@@ -4,16 +4,19 @@ pragma solidity >=0.6.10 <0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/KeeperCompatibleInterface.sol";
 import "../layerzero/interfaces/ILayerZeroEndpoint.sol";
+import "../utils/CoreUtility.sol";
 
 interface IScheduleRelayer {
     function lzEndpoint() external view returns (ILayerZeroEndpoint);
 
     function subLzChainID() external view returns (uint16);
 
+    function lastWeek() external view returns (uint256);
+
     function crossChainMint(bytes memory adapterParams) external payable;
 }
 
-contract CrossChainMintKeeperHelper is KeeperCompatibleInterface, Ownable {
+contract CrossChainMintKeeperHelper is KeeperCompatibleInterface, Ownable, CoreUtility {
     uint256 private constant DATA_LENGTH = 32; // abi.encode(uint256)
     uint256 private constant MINT_GAS_LIMIT = 100000;
 
@@ -21,13 +24,10 @@ contract CrossChainMintKeeperHelper is KeeperCompatibleInterface, Ownable {
     uint16 public immutable subLzChainID;
     ILayerZeroEndpoint public immutable lzEndpoint;
 
-    uint256 public lastTimestamp;
-
     constructor(address relayer_) public {
         relayer = IScheduleRelayer(relayer_);
         subLzChainID = IScheduleRelayer(relayer_).subLzChainID();
         lzEndpoint = IScheduleRelayer(relayer_).lzEndpoint();
-        _updateLastTimestamp(block.timestamp);
     }
 
     receive() external payable {}
@@ -37,22 +37,17 @@ contract CrossChainMintKeeperHelper is KeeperCompatibleInterface, Ownable {
         require(success, "ETH transfer failed");
     }
 
-    function updateLastTimestamp(uint256 lastTimestamp_) external onlyOwner {
-        _updateLastTimestamp(lastTimestamp_);
-    }
-
     function checkUpkeep(bytes calldata)
         external
         override
         returns (bool upkeepNeeded, bytes memory)
     {
-        upkeepNeeded = (block.timestamp > lastTimestamp + 1 weeks);
+        uint256 startWeek = _endOfWeek(block.timestamp) - 1 weeks;
+        uint256 lastWeek = relayer.lastWeek();
+        upkeepNeeded = (startWeek > lastWeek);
     }
 
     function performUpkeep(bytes calldata) external override {
-        uint256 lastTimestamp_ = lastTimestamp;
-        require(block.timestamp > lastTimestamp_ + 1 weeks, "Not yet");
-
         (uint256 srcFees, ) =
             lzEndpoint.estimateFees(
                 subLzChainID,
@@ -63,14 +58,5 @@ contract CrossChainMintKeeperHelper is KeeperCompatibleInterface, Ownable {
             );
         require(address(this).balance >= srcFees, "Not enough balance");
         relayer.crossChainMint{value: srcFees}(abi.encodePacked(uint16(1), MINT_GAS_LIMIT));
-
-        // Always skip to the lastest week
-        _updateLastTimestamp(
-            lastTimestamp_ + ((block.timestamp - lastTimestamp_ - 1) / 1 weeks) * 1 weeks
-        );
-    }
-
-    function _updateLastTimestamp(uint256 lastTimestamp_) private {
-        lastTimestamp = lastTimestamp_;
     }
 }
