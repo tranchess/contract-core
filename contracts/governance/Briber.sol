0d853bb33238b8ac3486ffa4a76474d10c135752
Bill Clippy <bill.clippy@tranchess.com>
2023-08-12 19:42:08 -0700

    Direclty bribe proposal

--- contracts/governance/Briber.sol
+++ contracts/governance/Briber.sol
@@ -37,6 +37,14 @@ contract Briber is Ownable {
 
     function bribe(address gauge) external onlyOwner {
         bytes32 proposal = keccak256(abi.encodePacked(gauge));
+        _bribe(proposal);
+    }
+
+    function bribe(bytes32 proposal) external onlyOwner {
+        _bribe(proposal);
+    }
+
+    function _bribe(bytes32 proposal) private {
         rewardClaimer.claimRewards();
         uint256 bribeAmount = IERC20(token).balanceOf(address(this));
         IERC20(token).safeApprove(bribeMarket.BRIBE_VAULT(), bribeAmount);
