25c86ca06f1f8ee2545baa06ea5541980890f1f8
Bill Clippy <bill.clippy@tranchess.com>
2024-03-26 20:33:24 -0400

    const oracle nit

--- contracts/oracle/ConstPriceOracle.sol
+++ contracts/oracle/ConstPriceOracle.sol
@@ -2,24 +2,23 @@
 pragma solidity >=0.6.10 <0.8.0;
 
 import "../interfaces/ITwapOracleV2.sol";
-import "../interfaces/IWstETH.sol";
 
-/// @title wstETH Price oracle
+/// @title Constant Price oracle
 /// @author Tranchess
-contract ConstantPriceOracle is ITwapOracleV2 {
+contract ConstPriceOracle is ITwapOracleV2 {
     uint256 public immutable price;
 
     constructor(uint256 price_) public {
         price = price_;
     }
 
-    /// @notice Return the latest price with 18 decimal places.
+    /// @notice Return the constant price with 18 decimal places.
     function getLatest() external view override returns (uint256) {
         return price;
     }
 
-    /// @notice For wstETH price oracle, we keep the `getTwap` interface
-    ///         compatible but it only returns the latest price.
+    /// @notice For constant price oracle, we keep the `getTwap` interface
+    ///         compatible but it only returns the constant price.
     function getTwap(uint256) external view override returns (uint256) {
         return price;
     }
